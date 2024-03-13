9da11bc24f14be3072c227e3664421a69ee09254
Matthews Offen <matthewsoffen2@gmail.com>
2024-03-13 12:38:47 +0200

    Remove reusable methods
    Moving resuable methods to be in a more general class(
    UiComponents class).

--- src/mdps_sms/Login.java
+++ src/mdps_sms/Login.java
@@ -1,13 +1,10 @@
 package mdps_sms;
 
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Tooltip;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -97,18 +94,18 @@ public class Login extends GridPane {
 		credentialsField.setSpacing(15);
 		
 		//buttons and icons
-		StackPane lockContainer = new StackPane(createIcon("lockWhite.png", 24));
+		StackPane lockContainer = new StackPane(UiComponents.createIcon("lockWhite.png", 24));
 		lockContainer.setMinSize(700, 100);
 		
-		login = createButton("logInWhite.png", 24, "login");
+		login = UiComponents.createButton("logInWhite.png", 24, "login");
 		StackPane loginContainer = new StackPane(login);
 		login.setOnAction(e -> verify());
 		
-		settings = createButton("settingsWhite.png", 24, "settings");
+		settings = UiComponents.createButton("settingsWhite.png", 24, "settings");
 		StackPane settingsContainer = new StackPane(settings);
 		settingsContainer.setMinSize(200, 200);
 		
-		cancel = createButton("cancelWhite.png", 24, "cancel[esc]");
+		cancel = UiComponents.createButton("cancelWhite.png", 24, "cancel[esc]");
 		StackPane cancelContainer = new StackPane(cancel);
 		cancelContainer.setMinSize(200, 200);
 		cancel.setOnAction(e -> clearFields());
@@ -137,34 +134,6 @@ public class Login extends GridPane {
 		this.setMaxWidth(700);
 		this.setClip(rect);
 		this.setStyle("-fx-background-color: #404040");
-	}
-	
-	public ImageView createIcon(String name, double iconSize) {
-		ImageView image = new ImageView(name);
-		image.setFitHeight(iconSize);
-		image.setFitWidth(iconSize);
-		return image;
-	}
-	
-	public Button createButton(String iconName, double iconSize, String tip) {
-		Button button = new Button();
-		button.setGraphic(createIcon(iconName, iconSize));
-		button.setStyle("-fx-background-color: #e3dedb08;");
-		button.setPadding(new Insets(5));
-		button.setTooltip(new Tooltip(tip));
-		button.getTooltip().setFont(Font.font("ubuntu", 14));
-		
-		//register events
-		button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: #e3dedb5f;"));
-		button.setOnMouseExited(e -> button.setStyle("-fx-background-color: #e3dedb08;"));
-		
-		//round corner effect
-		/*Rectangle rect = new Rectangle(iconSize + 10, iconSize +10);
-		rect.setArcHeight(30);
-		rect.setArcWidth(30);
-		button.setClip(rect);*/
-		
-		return button;
 	}
 	
 	public void clearFields() {
