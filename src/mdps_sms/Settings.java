267816765d2bd6980c200375b68f8c72c8475a79
Matthews Offen <matthewsoffen2@gmail.com>
2024-03-25 13:34:14 +0200

    first settings draft

--- src/mdps_sms/Settings.java
+++ src/mdps_sms/Settings.java
@@ -3,93 +3,139 @@ package mdps_sms;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 
+/**
+ * This class is used for making instances of the Gui settings object.
+ * The object helps the user in configuring personal preferred settings which 
+ * overrides the defaults. 
+ * It also has a restore option which can be used to revert the settings to their
+ * default values in case the user messed up.
+ */
 public class Settings extends BorderPane {
-	private Label header = new Label("Settings");
-	private Button restore = new Button("Restore");
-	private Separator horizontalLine = new Separator();
-	private GridPane topHeader = new GridPane(); 
 	
-	ScrollPane scrBar = new ScrollPane(this.getCenter());
+	//top header
+	Label forSettings =  new Label("Settings");
+	Button restore = new Button("Restore");
+	Separator separate = new Separator();
+	HBox liner = new HBox(forSettings, restore);
 	
+	//first card
+	Label changeAccount = new Label("Change account details");
+	//--------------------------------------------------------
+	Label forAccountName = new Label("account name");
+	TextField name = new TextField();
+	VBox namePair = new VBox(forAccountName, name);
+	//--------------------------------------------------------
+	Label forAccountEmail = new Label("account email");
+	TextField email = new TextField();
+	VBox emailPair = new VBox(forAccountEmail, email);
 	
-	private VBox centerBody = new VBox();
+	Label changePassword = new Label("Change password details");
+	//---------------------------------------------------------
+	Label forOldPassword = new Label("old password");
+	PasswordField oldPassword = new PasswordField();
+	VBox oldPair = new VBox(forOldPassword, oldPassword);
+	//---------------------------------------------------------
+	Label forNewPassword = new Label("new password");
+	PasswordField newPassword = new PasswordField();
+	VBox newPair = new VBox(forNewPassword, newPassword);
 	
-	private Button cancel = new Button("Cancel");
-	private Button delete = new Button("Delete Account");
-	private Button save = new Button("Save");
-	private GridPane footer = new GridPane();
+	Card generalSettings = new Card("General");
 	
-	public Settings() {
-		//for top heading
-		header.setFont(Font.font("Outfit SemiBold", 24));
-		header.setTextFill(Color.valueOf("#232323"));
+	Settings(){
+		
+		forSettings.setFont(Font.font("Outfit SemiBold", 24));
+		forSettings.setTextFill(Color.web("#232323"));
 		
 		restore.setFont(Font.font("Outfit SemiBold", 16));
 		restore.setTextFill(Color.WHITE);
 		restore.setStyle("-fx-background-color: #232323");
-		restore.setMinWidth(100);
+		liner.setPadding(new Insets(25, 0, 15, 0));
+		liner.setMaxWidth(710);
+		liner.setSpacing(531);
 		
-		horizontalLine.setMinWidth(500);
-		horizontalLine.setMinHeight(5);
-		horizontalLine.setStyle("-fx-background-color: #232323");
 		
-		topHeader.add(header, 0, 0);
-		topHeader.add(restore, 1, 0);
-		topHeader.add(horizontalLine, 0, 1);
-		topHeader.setAlignment(Pos.CENTER);
-		GridPane.setHalignment(restore, HPos.RIGHT);
-		GridPane.setValignment(header, VPos.CENTER);
-		GridPane.setColumnSpan(horizontalLine, 2);
-		topHeader.setVgap(10);
-		topHeader.setHgap(500);
-		topHeader.setMaxWidth(700);
-	//	topHeader.setStyle("-fx-background-color: red");
+		//style sub headers
+		for(Label elem : new Label[]{changeAccount, changePassword}) {
+			elem.setFont(Font.font("Outfit SemiBold", 16));
+			elem.setTextFill(Color.WHITE);
+		}
+		//style points
+		for(Label elem : new Label[]{forAccountName, forAccountEmail, forOldPassword, forNewPassword}) {
+			elem.setFont(Font.font("Outfit", 16));
+			elem.setTextFill(Color.WHITE);
+		}
+		//style fields
+		for(TextField elem : new TextField[]{name, email, oldPassword, newPassword}) {
+			elem.setFont(Font.font("ubuntu", 14));
+			elem.setMinWidth(250);
+			elem.setMinHeight(40);
+			elem.setStyle("-fx-background-color: #484848; -fx-text-fill: white");
+		}
 		
-		//for center or main body
+		name.setPromptText("enter name ...");
+		name.setText("Admin");
+		email.setPromptText("enter email address ...");
+		email.setText("Admin@yahoo.co.uk");
+		namePair.setSpacing(5);
+		emailPair.setSpacing(5);
+		generalSettings.board.add(changeAccount, 0, 0);
+		generalSettings.board.add(namePair, 0, 1);
+		generalSettings.board.add(emailPair, 1, 1);
+		generalSettings.board.setHgap(150);
 		
-		//for footer / action buttons
-		cancel.setFont(Font.font("Outfit SemiBold", 16));
-		cancel.setTextFill(Color.WHITE);
-		cancel.setStyle("-fx-background-color: #232323");
-		cancel.setMinWidth(100);
+		changePassword.setPadding(new Insets(50,0,0,0));
+		oldPassword.setPromptText("enter old password ...");
+		newPassword.setPromptText("enter new password ...");
+		oldPair.setSpacing(5);
+		newPair.setSpacing(5);
+		generalSettings.board.add(changePassword, 0, 2);
+		generalSettings.board.add(oldPair, 0, 3);
+		generalSettings.board.add(newPair, 1, 3);
+		generalSettings.board.setHgap(150);
+		generalSettings.board.setVgap(5);
+		Rectangle genRec = new Rectangle(700, 300);
+		genRec.setArcHeight(30);
+		genRec.setArcWidth(30);
+		generalSettings.board.setClip(genRec);
 		
-		delete.setFont(Font.font("Outfit SemiBold", 16));
-		delete.setTextFill(Color.WHITE);
 		
-		delete.setStyle("-fx-background-color: #CB1E1E");
-		delete.setMinWidth(150);
 		
-		save.setFont(Font.font("Outfit SemiBold", 16));
-		save.setTextFill(Color.WHITE);
-		save.setStyle("-fx-background-color: #232323");
-		save.setMinWidth(100);
+		this.setTop(liner);
+		this.setCenter(generalSettings);
+		BorderPane.setAlignment(liner, Pos.CENTER);
+	}
+}
+
+class Card extends VBox{
+	Label cardLabel = new Label();
+	GridPane board = new GridPane();
+	
+	Card(String label){
+		cardLabel.setText(label);
 		
-		//footer.add(cancel, 0, 0);
-		footer.add(delete, 0, 0);
-		footer.add(save, 1, 0);
-		footer.setAlignment(Pos.CENTER);
-		GridPane.setHalignment(save, HPos.RIGHT);
-		footer.setHgap(440);
-		footer.setMaxWidth(700);
-	//	footer.setStyle("-fx-background-color: red");
+		//default label and board style
+		cardLabel.setFont(Font.font("Outfit SemiBold", 20));
+		cardLabel.setTextFill(Color.web("#232323"));
 		
-		this.setTop(new StackPane(topHeader));
-		this.setCenter(centerBody);
-		this.setBottom(footer);
-		this.setPadding(new Insets(40,0,10,0));
-		this.setAlignment(footer, Pos.CENTER);
+		board.setStyle("-fx-background-color: #232323");
+		board.setPadding(new Insets(30));
+		board.setMaxWidth(700);
+		
+		this.getChildren().addAll(cardLabel, board);
+		this.setSpacing(10);
+		this.setMaxWidth(700);
 	}
 }
